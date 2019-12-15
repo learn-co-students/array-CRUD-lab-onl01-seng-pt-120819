b83692a3ced2b4a13aacbf07fac5ccea1d327b58
Karen Daniela <kdaniimora@gmail.com>
2019-12-15 22:35:29 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_puppies = ["Gaia", "Luna","Whiskey", "Thor"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_puppies ["Gaia", "Luna", "estrellita"]
+  my_puppies<< "thor"
+ 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   my_puppies ["Gaia", "Luna","estrellita"]
+  my_puppies.push ("thor")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  puppy = ["Gaia"]
+  Gaia= puppy.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  puppy = ["Gaia"]
+  Gaia= puppy.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  puppies = ["Luna", "Whiskey"]
+  puppies=[1]
+  puppies=[0]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  puppy = ["Gaia"]
+  puppy.index_number [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  puppies = ["Luna", "Whiskey"]
+ puppies =-1
 end
